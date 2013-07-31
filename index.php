5fcac392853aee50d1d2e93fecd269df57b1a8b7
Turak <aurelien@lourdel.fr>
2013-07-31 20:53:50 +0200

    ajout index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+//insertion du header
+include("include/header.php");
+
+//Insertion du menu
+include("include/menu.php");
+
+//Partie texte du site
+echo "partie centrale";
+//Fin texte du site
+
+//Insertion du footer
+include("include/footer.php");
+
+?>
