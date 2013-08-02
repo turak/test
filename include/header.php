a547af565bee80c212fd03cb0f4143e5d284bbef
aurelien <aurelien@lourdel.fr>
2013-08-02 14:54:57 +0200

    insertion css menu.css

--- include/header.php
+++ include/header.php
@@ -2,6 +2,7 @@
 <head>
 	<title>Tout sur la mise en place d'un serveur Debian</title>
 	<link href='css/style.css' rel='stylesheet' type='text/css'/>
+	<link href='css/menu.css' rel='stylesheet' type='text/css'/>
 	<link href='http://fonts.googleapis.com/css?family=Crafty+Girls' rel='stylesheet' type='text/css'>
 </head>
 <body>
