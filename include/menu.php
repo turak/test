f453f7a56e33f44f2de66dd9e02ea295ed605783
aurelien <aurelien@lourdel.fr>
2013-08-02 15:11:19 +0200

    maj faute de frappe

--- include/menu.php
+++ include/menu.php
@@ -1,7 +1,7 @@
 <div id="menusite" class="menuvertical">
         <ul id="menu" class="menu">
                 <li class="Rubrique"><a href="index.php">Accueil</a>
-                <li class="Rubrique"><a href="index.php?insall">Installation</a>
+                <li class="Rubrique"><a href="index.php?install">Installation</a>
                 <li class="Rubrique"><a href="index.php?mode=photos">Administration</a>
                 <li class="Rubrique"><a href="index.php">Tutoriels</a>
                 <li class="Rubrique"><a href="index.php?contact">Contact</a></li>
